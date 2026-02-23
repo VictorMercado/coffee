461cb8d01e5a31841c85ae153038e52376758873
Victor Mercado <victormercado@outlook.com>
2026-02-23 13:40:19 -0800

    constants

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+// Hardcoded guest user constants
+// The guest account is shared by all unauthenticated users
+export const GUEST_USER_ID = "cml5kyuk50001lv7t4uumqgyj";
+export const GUEST_USERNAME = "guest";
+export const GUEST_SESSION = {
+  id: GUEST_USER_ID,
+  username: "guest",
+  role: "USER",
+};
